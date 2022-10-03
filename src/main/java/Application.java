4d7b77702d3360a5419ac0418bd9b66a71c7d055
Rita Lopes <up202108819@up.pt>
2022-10-03 15:50:09 +0100

    Added x on window

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,5 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,12 +10,20 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         try {
-            Terminal terminal = new
-                    DefaultTerminalFactory().createTerminal();
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory()
+                    .setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
             Screen screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+                    [0]);
+            screen.refresh();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
