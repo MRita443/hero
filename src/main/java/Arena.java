5c341e6d0a488570cacfc7c5802b0d3124cc9c3b
Rita Lopes <up202108819@up.pt>
2022-10-09 15:56:54 +0100

    Added Class Coin

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -6,6 +6,7 @@ import com.googlecode.lanterna.input.KeyStroke;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class Arena {
@@ -13,11 +14,14 @@ public class Arena {
     private Hero hero;
     private List<Wall> walls;
 
+    private List<Coin> coins;
+
     public Arena(int width, int height, Hero hero) {
         this.width = width;
         this.height = height;
         this.hero = hero;
         this.walls = createWalls();
+        this.coins = createCoins();
     }
 
     public void draw(TextGraphics graphics) {
@@ -25,13 +29,15 @@ public class Arena {
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         for (Wall wall : walls)
             wall.draw(graphics);
+        for (Coin coin : coins)
+            coin.draw(graphics);
         hero.draw(graphics);
     }
 
 
     private void moveHero(Position position) {
-        if (canHeroMove(position))
-            hero.setPosition(position);
+        if (canHeroMove(position)) hero.setPosition(position);
+        retrieveCoins(hero, coins);
     }
 
     private boolean canHeroMove(Position position) {
@@ -64,6 +70,32 @@ public class Arena {
         }
         return walls;
     }
+
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int nextX = random.nextInt(width - 2) + 1, nextY = random.nextInt(height - 2) + 1;
+            Coin nextCoin = new Coin(nextX, nextY);
+            while (coins.contains(nextCoin) || hero.getPosition() == nextCoin.getPosition()) {
+                nextX = random.nextInt(width - 2) + 1;
+                nextY = random.nextInt(height - 2) + 1;
+                nextCoin.setPosition(new Position(nextX, nextY));
+            }
+            coins.add(nextCoin);
+        }
+        return coins;
+    }
+
+    private void retrieveCoins(Hero hero, List<Coin> coins) {
+        for (Coin coin : coins) {
+            if (hero.getPosition().equals(coin.getPosition())) {
+                coins.remove(coin);
+                break;
+            }
+        }
+    }
+
 }
 
 
